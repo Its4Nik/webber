3d16c54f65d3c24e221f36fc0c1f5973fcf38d83
Its4Nik <106100177+Its4Nik@users.noreply.github.com>
2023-11-23 08:30:35 +0100

    Update webber.sh
    Update to 0.4

--- webber.sh
+++ webber.sh
@@ -6,16 +6,22 @@ DATE_FORMAT="%d.%m.%Y"
 INDEX_SCRIPT="./code/boilerplates/index"
 POST_SCRIPT="./code/boilerplates/posts"
 
+# Colors for formatting
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
 # Functions
 
 # Function to initialize or renew the configuration
 setup() {
     if [ -f "$CONFIG_FILE" ]; then
-        echo "Configuration found:"
+        echo -e "${GREEN}Configuration found:${NC}"
         display_config
     else
-        echo "------------------------------------------"
-        echo "Running initial setup..."
+        echo -e "${YELLOW}------------------------------------------${NC}"
+        echo -e "${YELLOW}Running initial setup...${NC}"
         touch "$CONFIG_FILE"
 
         read -p "Please enter the absolute path for input files: " input
@@ -31,28 +37,28 @@ setup() {
 # Function to prompt the user for actions
 prompt() {
     echo
-    echo "Thanks for using Webber!"
-    echo "Press q: Save and quit"
-    echo "Press c: Exit without saving"
-    echo "Press s: Save and continue"
-    echo "Press r: Redo Setup"
-    echo "Press u: Uninstall"
+    echo -e "Thanks for using ${YELLOW}Webber!${NC}"
+    echo -e "Press ${YELLOW}q:${NC} Save and quit"
+    echo -e "Press ${YELLOW}c:${NC} Exit without saving"
+    echo -e "Press ${YELLOW}s:${NC} Save and continue"
+    echo -e "Press ${YELLOW}r:${NC} Redo Setup"
+    echo -e "Press ${YELLOW}u:${NC} Uninstall"
     echo
     read -n 1 state
     echo
 
     case "$state" in
     q)
-        echo "Saving and exiting"
+        echo -e "${GREEN}Saving and exiting${NC}"
         exit 0
         ;;
     c)
-        echo "Exiting without saving"
+        echo -e "${GREEN}Exiting without saving${NC}"
         rm "$CONFIG_FILE"
         exit 0
         ;;
     s)
-        echo "Continuing..."
+        echo -e "${GREEN}Continuing...${NC}"
         ;;
     r)
         rm "$CONFIG_FILE"
@@ -62,7 +68,7 @@ prompt() {
         uninstall
         ;;
     *)
-        echo "No known argument provided, try again"
+        echo -e "${RED}No known argument provided, try again${NC}"
         prompt
         ;;
     esac
@@ -70,31 +76,31 @@ prompt() {
 
 # Function to uninstall Webber
 uninstall() {
-    echo "Uninstalling, are you sure?"
+    echo -e "${YELLOW}Uninstalling, are you sure?${NC}"
     read -p "Type \"Webber\" to uninstall, to exit press CTRL+C: " -n 6 uninstall
     echo
 
     if [ "$uninstall" = "Webber" ]; then
-        echo "Uninstalling..."
+        echo -e "${GREEN}Uninstalling...${NC}"
         rm -rf ./code
         rm ./webber.sh
-        echo "Done"
+        echo -e "${GREEN}Done${NC}"
         exit 0
     else
-        echo "Exiting (not uninstalling)"
+        echo -e "${GREEN}Exiting (not uninstalling)${NC}"
         exit 1
     fi
 }
 
 # Function to display the current configuration
 display_config() {
-    echo ".env found"
-    echo "To use different directories run \"./webber.sh --renew\""
+    echo -e "${GREEN}.env found${NC}"
+    echo -e "To use different directories run \"./webber.sh --renew\""
     input="$(head -n 1 "$CONFIG_FILE")"
     output="$(tail -n 1 "$CONFIG_FILE")"
-    echo "Input Path: $input"
-    echo "Output Index: ${output}index.html"
-    echo "Output Posts: ${output}posts/post1.html"
+    echo -e "${YELLOW}Input Path:${NC} $input"
+    echo -e "${YELLOW}Output Index:${NC} ${output}index.html"
+    echo -e "${YELLOW}Output Posts:${NC} ${output}posts/post1.html"
     echo
 }
 
@@ -103,30 +109,31 @@ create_overview() {
     mkdir -p "${output}posts/"
 
     echo
-    echo "Creating Overview Page"
+    echo -e "${YELLOW}Creating Overview Page${NC}"
     bash "$INDEX_SCRIPT/1.sh" "$output/index.html"
 
-    for unProcessed in "${input}"*.md; do
-        baseUnProcessed="${unProcessed%.*}"
-        outputFormat="$(date +"$DATE_FORMAT")-${baseUnProcessed##*/}"
+    echo "starting"
 
-        for processed in "${output}"*.html; do
-            echo "Adding $unProcessed to $output"
-            echo
-            info="$(head -n 1 "$unProcessed")"
-            bash "$INDEX_SCRIPT/2.sh" "$output/index.html" "${baseUnProcessed##*/}" "$unProcessed"
-        done
+    for file in $(ls -r "${output}posts/"*.html); do
+        echo "$file"
+        index="${output}index.html"
+        base="$(basename "${file%.*}")"
+        info="${input}${base:11}.md"
+        echo "$INDEX_SCRIPT/2.sh $index $base"
+        bash $INDEX_SCRIPT/2.sh $index $base $info
+        echo "Done: $file"
+
     done
-
     bash "$INDEX_SCRIPT/3.sh" "$output/index.html"
 }
 
 # Function to process and create posts
+# Function to process and create posts
 process_posts() {
-    for unProcessed in "${input}"*.md; do
+    for unProcessed in $(ls -r "${input}"*.md); do  # Removed double quotes around the command substitution
         run="true"
 
-        for processed in "${output}posts/"*.html; do
+        for processed in $(ls -r "${output}posts/"*.html); do  # Removed double quotes around the command substitution
             baseProcessed="$(basename "${processed%.*}")"
             cutBaseProcessed="${baseProcessed:11}"
             baseUnProcessed="$(basename "${unProcessed%.*}")"
@@ -138,34 +145,35 @@ process_posts() {
         done
 
         if [ "$run" == "true" ]; then
-            echo "Processing: $unProcessed"
+            echo -e "${YELLOW}Processing:${NC} $unProcessed"
             bash "$POST_SCRIPT/1.sh" "$unProcessed" "${output}"
             bash "$POST_SCRIPT/2.sh" "$unProcessed" "${output}"
             bash "$POST_SCRIPT/3.sh" "$unProcessed" "${output}"
         else
-            echo "Skipping: $unProcessed"
+            echo -e "${YELLOW}Skipping:${NC} $unProcessed"
         fi
     done
 }
 
+
 # Main Script
 
-if [ "$1" = "--renew" ]; then
-    echo "Renewing your config, are you sure?"
+if [ "$1" = "-renew" ]; then
+    echo -e "${YELLOW}Renewing your config, are you sure?${NC}"
     read -p "y/n: " -n 1 confirm
     echo
 
     if [ "$confirm" = "y" ]; then
-        echo "Renewing..."
+        echo -e "${GREEN}Renewing...${NC}"
         echo
         rm "$CONFIG_FILE"
         setup
     elif [ "$confirm" = "n" ]; then
-        echo "Exiting"
+        echo -e "${GREEN}Exiting${NC}"
         echo
         exit 0
     else
-        echo "UNKNOWN INPUT ($confirm) EXITING"
+        echo -e "${RED}UNKNOWN INPUT ($confirm) EXITING${NC}"
         exit 1
     fi
 fi
@@ -175,12 +183,11 @@ if [ "$1" = "-u" ]; then
 fi
 
 setup
-create_overview
 process_posts
+create_overview
 
 echo
 echo
-echo "Finished"
-echo "Thanks for using Webber"
-echo "~ItsNik"
-echo
+echo -e "${GREEN}Finished${NC}"
+echo -e "${GREEN}Thanks for using Webber${NC}"
+echo -e "${YELLOW}~ItsNik${NC}"
